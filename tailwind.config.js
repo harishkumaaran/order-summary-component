373ef7e73c6ec8c60e826a65b07362084cb3f9f0
harish <harishkumaaran23@gmail.com>
2021-09-07 21:11:03 +0530

    Bug fix scaling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'width': '350px',
+        'height': '600px'
+      }
+    },
   },
   variants: {
     extend: {},
